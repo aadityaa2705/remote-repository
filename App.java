2810370e03059d6f61c4412a0072fea45ed5c0d4
Aditya Barathe <91825168+aadityaa2705@users.noreply.github.com>
2023-12-13 16:18:20 +0530

    Update App.java

--- App.java
+++ App.java
@@ -2,7 +2,7 @@ public class App{
 	public static void main(String[] args){
 		int a =0;
 		int b=1;
-		for(int i =0; i < 13; i++){
+		for(int i =0; i < 15; i++){
 			System.out.print(a+" ");
 			int c = a +b;
 			a = b;
